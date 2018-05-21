77c21e8337547529bd798f9a9a4c7540736f4164
rznamerovskyi <r.znamerovskiy@gmail.com>
2018-05-21 11:19:36 +0300

    lalala

---
+++ src/SimpleDotComTestDrive.java
@@ -0,0 +1,19 @@
+public class SimpleDotComTestDrive {
+    public static void main (String[] args) {
+        DotCom dot = new DotCom();
+
+        int[] locations = {2, 3, 4};
+        dot.setLocationCells(locations);
+
+        String userGuess = "2";
+
+        String result = dot.checkYourself(userGuess);
+
+        String testResult = "Неудача";
+        if (result.equals("Попал")) {
+            testResult = "Пройден";
+        }
+
+        System.out.println(testResult);
+    }
+}
---
+++ src/differentTaskFromBook/MultiFor.java
@@ -0,0 +1,32 @@
+package differentTaskFromBook;
+
+public class MultiFor {
+    public static void main(String[] args) {
+
+        for(int x = 0; x < 4; x++) {
+            if (x == 1) {
+                x++;
+            }
+            for(int y = 4; y > 2; y--) {
+                System.out.println(x + " " + y);
+            }
+
+
+        }
+
+    }
+}
+
+/*
+mylist[0] = a;
+mylist[1] = b;
+
+mylist.length();
+
+Object o = mylist[1];
+
+???? do not exist
+
+boolean isIn;
+for (int i = 0; i <
+ */
